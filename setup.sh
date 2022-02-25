4cd993700ab7d1586348e3c818fec732e7086402
tjorbengross <83009395+tjorbengross@users.noreply.github.com>
2022-02-25 02:09:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,10 +10,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
- sudo apt-get update
- sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
  
  
- wget https://vstsagentpackage.azureedge.net/agent/2.200.2/vsts-agent-linux-x64-2.200.2.tar.gz
- mkdir myagent && cd myagent
- tar zxvf ../vsts-agent-linux-x64-2.200.2.tar.gz
+wget https://vstsagentpackage.azureedge.net/agent/2.200.2/vsts-agent-linux-x64-2.200.2.tar.gz
+mkdir myagent && cd myagent
+tar zxvf ../vsts-agent-linux-x64-2.200.2.tar.gz
+sudo chmod 666 /var/run/docker.sock
